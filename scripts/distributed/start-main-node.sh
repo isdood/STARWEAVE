30ddf0a79dda3709a1591d57f958b495a3a81a70
isdood <calebjdt@proton.me>
2025-09-10 15:49:31 -0400

    Debugging automated starting of main/worker nodes

--- scripts/distributed/start-main-node.sh
+++ scripts/distributed/start-main-node.sh
@@ -16,29 +16,16 @@ DIST_MAX=9100
 echo "Starting STARWEAVE Main Node on $HOSTNAME..."
 echo "HTTP server will be available on port $HTTP_PORT"
 
-echo "Starting Erlang distribution node..."
-# Start the Erlang node with distribution settings in the background
-(
-  erl \
-    -sname $NODE_NAME \
-    -setcookie $COOKIE \
-    -kernel inet_dist_listen_min $DIST_MIN \
-    -kernel inet_dist_listen_max $DIST_MAX \
-    -eval 'io:format("Main node started. Waiting for connections...").' \
-    -s init stop &
-)
+# Set the PORT environment variable for Phoenix
+export PORT=$HTTP_PORT
 
-# Give the Erlang node a moment to start
-sleep 2
-
+# Start the Phoenix web server with distribution settings
 echo "Starting Phoenix web server..."
-# Start the Phoenix web server
 iex \
-  --sname ${NODE_NAME}_web \
+  --sname $NODE_NAME \
   --cookie $COOKIE \
   --erl "-kernel inet_dist_listen_min $DIST_MIN inet_dist_listen_max $DIST_MAX" \
-  -S mix phx.server \
-  --http-port $HTTP_PORT
+  -S mix phx.server
 
 echo "Main node $NODE_NAME@$HOSTNAME started."
 echo "Web interface available at http://$HOSTNAME:$HTTP_PORT"
